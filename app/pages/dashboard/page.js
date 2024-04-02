cda59208be3cfa2db9381b9051d4e7db08430860
Tobdhen <114719327+nedbot1@users.noreply.github.com>
2024-04-02 10:06:00 +0600

    made changes in dashboard, login, signin

--- app/pages/dashboard/page.js
+++ app/pages/dashboard/page.js
@@ -1,11 +1,65 @@
 "use client";
 
-export default function dashboard() {
+import { useState } from "react";
+
+export default function Dashboard() {
+  const [tourPackages, setTourPackages] = useState([]);
+
+  const fetchTourPackages = async () => {
+    try {
+      const response = await fetch("/api/tour_packages", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch tour packages");
+      }
+      const data = await response.json();
+      setTourPackages(data.tour_packages);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center bg-black-50 py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-full flex justify-center py-2 px-4 border  rounded-md shadow-sm text-sm font-medium text-white">
-        WELLCOME
+    <div>
+      <div className="max-w-md w-full mt-5 ">
+        <button
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+          type="button"
+          onClick={fetchTourPackages}
+        >
+          TOUR PACKAGES
+        </button>
       </div>
+      {/* Display packages here */}
+      {tourPackages.length > 0 && (
+        <div className="text-white border-1 border-white bg-green">
+          <ul>
+            {tourPackages.map((tourPackage) => (
+              <>
+                <div className="my-auto mx-10">
+                  <li key={tourPackage.id} className="mt-4">
+                    TITLE: {tourPackage.title}
+                  </li>
+                  <li key={tourPackage.id} className="mt-4">
+                    DESCRIPTION: {tourPackage.description}
+                  </li>
+                  <li key={tourPackage.id} className="mt-4">
+                    PRICE: {tourPackage.price}
+                  </li>
+                  <li key={tourPackage.id} className="mt-4">
+                    DURATION: {tourPackage.duration}
+                  </li>
+                </div>
+              </>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
