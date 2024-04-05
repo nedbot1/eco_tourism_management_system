8a890420e26f7a8ba07c29d8ee404d13da79d835
Chakar-Pema-Nyingpo <114732678+Chakar-Pema-Nyingpo@users.noreply.github.com>
2024-04-05 10:01:48 +0600

    made changes in UI

--- app/page/cars/page.js
+++ app/page/cars/page.js
@@ -2,27 +2,27 @@ export default function car(){
     return(
         <>
     <div className="w-full h-full bg-white ">  
-        <div className=" w-full h-72 backdrop-opacity-10 backdrop-invert bg-green-950/30 py-5 flex justify-evenly px-5">  
+        <div className=" w-full h-72 bg-green-950 hover:bg-green-950/30 py-5 flex justify-evenly px-5 rounded-xl">  
             <img src='/car/C.jpg' className="ml-4 mr-4 h-60 w-60"/>
             <p className="text-3xl"> Hyundai, Creta</p>
         </div>
 
-        <div className="mt-5 w-full h-72 backdrop-opacity-10 backdrop-invert bg-green-950/30 py-5 flex justify-evenly px-5">  
+        <div className="mt-5 w-full h-72 bg-green-950 hover:bg-green-950/30 py-5 flex justify-evenly px-5 rounded-xl">  
             <img src='/car/K.jpg' className="ml-4 mr-4 h-60 w-60"/>
             <p className="text-3xl"> Kia, Sorento</p>
         </div>
 
-        <div className="mt-5 w-full h-72 backdrop-opacity-10 backdrop-invert bg-green-950/30 py-5 flex justify-evenly px-5">  
+        <div className="mt-5 w-full h-72 bg-green-950 hover:bg-green-950/30 py-5 flex justify-evenly px-5 rounded-xl">  
             <img src='/car/KS.jpg' className="ml-4 mr-4 h-60 w-60"/>
             <p className="text-3xl"> Kia, Seltos</p>
         </div>
 
-        <div className="mt-5 w-full h-72 backdrop-opacity-10 backdrop-invert bg-green-950/30 py-5 flex justify-evenly px-5">  
+        <div className="mt-5 w-full h-72 bg-green-950 hover:bg-green-950/30 py-5 flex justify-evenly px-5 rounded-xl">  
             <img src='/car/P.jpg' className="ml-4 mr-4 h-60 w-60"/>
             <p className="text-3xl">Toyota, Pardo</p>
         </div>
 
-        <div className="mt-5 w-full h-72 backdrop-opacity-10 backdrop-invert bg-green-950/30 py-5 flex justify-evenly px-5">  
+        <div className="mt-5 w-full h-72 bg-green-950 hover:bg-green-950/30 py-5 flex justify-evenly px-5 rounded-xl">  
             <img src='/car/R.jpg' className="ml-4 mr-4 h-60 w-60"/>
             <p className="text-3xl">Toyota, Rush</p>
         </div>
