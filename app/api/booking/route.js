85e9b1e7c83367e72d923ad4dc845913940bf8b8
Tobdhen <114719327+nedbot1@users.noreply.github.com>
2024-04-10 14:31:17 +0600

    some changes on ui

--- app/api/booking/route.js
+++ app/api/booking/route.js
@@ -1,7 +1,7 @@
 import knex from "@/database";
 
 export async function GET() {
-  const booking = await knex("bookings").select("*");
+  const booking = await knex("bookings").join.select("*");
   return new Response(JSON.stringify({ booking }), {
     status: 200,
   });
