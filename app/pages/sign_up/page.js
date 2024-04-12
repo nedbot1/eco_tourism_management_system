ae90f8403eec30bf5defad35c6f0582e98515bd8
Tobdhen <114719327+nedbot1@users.noreply.github.com>
2024-04-12 11:55:56 +0600

    made changes in login and sign in page

--- app/pages/sign_up/page.js
+++ app/pages/sign_up/page.js
@@ -47,11 +47,11 @@ const SignUp = () => {
     <div
       className="min-h-screen flex items-center justify-center bg-image py-12 px-4 sm:px-6 lg:px-8"
       style={{
-        backgroundImage: `url('https://images.unsplash.com/photo-1608659377506-3b4fec4f7634?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
-        backgroundSize: "cover",
+        backgroundImage: `url(/LSBG.jpg)`,
         minHeight: "100vh",
         backgroundColor: "#CBD5E0",
         backgroundPosition: "center",
+        backgroundSize: "cover",
       }}
     >
       <div className="max-w-md w-full space-y-8">
@@ -66,7 +66,7 @@ const SignUp = () => {
           )}
         </div>
         <form
-          className="mt-8 space-y-6 bg-transparent/15 p-16 rounded-xl"
+          className="mt-8 space-y-6 bg-transparent/30 p-16 rounded-xl"
           onSubmit={handleSignUp}
         >
           <div className="rounded-md shadow-sm space-y-6">
